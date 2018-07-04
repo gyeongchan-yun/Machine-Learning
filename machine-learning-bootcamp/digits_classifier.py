492d94802e83a3402810461700419fc3ec5509ec
uni20141379 <rugyoon@gmail.com>
2018-07-04 16:36:13 +0900

    ADD random forest classifier example

--- machine-learning-bootcamp/digits_classifier.py
+++ machine-learning-bootcamp/digits_classifier.py
@@ -1,36 +1,64 @@
 import numpy as np
-from sklearn import datasets, tree, metrics
+from sklearn import datasets, tree, metrics, ensemble
 
-digits = datasets.load_digits()
-# print("digits: {0}".format(digits))
+import sys
 
-# List that stores location of data of 3 and 8
-flag_3_8 = (digits.target == 3) + (digits.target == 8)
-# print("flag_3_8: {0}".format(flag_3_8))
 
-images = digits.images[flag_3_8]
-labels = digits.target[flag_3_8]
+def implement_classifier(classifier):
+    digits = datasets.load_digits()
+    # print("digits: {0}".format(digits))
 
-# 1-dimensioning
-images = images.reshape(images.shape[0], -1)
+    # == List that stores location of data of 3 and 8
+    flag_3_8 = (digits.target == 3) + (digits.target == 8)
+    # print("flag_3_8: {0}".format(flag_3_8))
 
-n_samples = len(flag_3_8[flag_3_8])
-# print("flag_3_8[flag_3_8]: {0}".format(flag_3_8[flag_3_8]))
-train_size = int(n_samples * (3/5))
-classifier = tree.DecisionTreeClassifier()
-classifier.fit(images[:train_size], labels[:train_size])
+    images = digits.images[flag_3_8]
+    labels = digits.target[flag_3_8]
 
-expected_data = labels[train_size:]
-predicted_data = classifier.predict(images[train_size:])
+    # == 1-dimensioning
+    images = images.reshape(images.shape[0], -1)
 
-# == Performance of classifier
-print("Accuracy: ", metrics.accuracy_score(expected_data, predicted_data))
+    n_samples = len(flag_3_8[flag_3_8])
+    # print("flag_3_8[flag_3_8]: {0}".format(flag_3_8[flag_3_8]))
+    train_size = int(n_samples * (3/5))
 
-print("\nConfusion Matrix: \n", metrics.confusion_matrix(expected_data, predicted_data))
+    classifier.fit(images[:train_size], labels[:train_size])
 
-print("\nPrecision: ", metrics.precision_score(expected_data, predicted_data, pos_label=3))
+    expected_data = labels[train_size:]
+    predicted_data = classifier.predict(images[train_size:])
 
-print("\nRecall: ", metrics.recall_score(expected_data, predicted_data, pos_label=3))
+    # == Performance of classifier
+    print("Accuracy: ", metrics.accuracy_score(expected_data, predicted_data))
 
-print("\nF-measure: ", metrics.f1_score(expected_data, predicted_data, pos_label=3))
+    print("\nConfusion Matrix: \n", metrics.confusion_matrix(expected_data, predicted_data))
 
+    print("\nPrecision: ", metrics.precision_score(expected_data, predicted_data, pos_label=3))
+
+    print("\nRecall: ", metrics.recall_score(expected_data, predicted_data, pos_label=3))
+
+    print("\nF-measure: ", metrics.f1_score(expected_data, predicted_data, pos_label=3))
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python digits_classifier.py index")
+        sys.exit(0)
+
+    index = int(sys.argv[1])
+
+    if index == 0:
+        classifier = tree.DecisionTreeClassifier()
+    elif index == 1:
+        classifier = tree.DecisionTreeClassifier(max_depth=3)
+    elif index == 2:
+        # == n_estimators: number of weak learners, criterion: decision tree algorithm
+        # == random forest - bagging algorithm 
+        classifier = ensemble.RandomForestClassifier(n_estimators=20, max_depth=3, criterion="gini")
+    else:
+        print("No classifier\n")
+        sys.exit(0)
+
+    implement_classifier(classifier)
+
+if __name__ == "__main__":
+    main()
